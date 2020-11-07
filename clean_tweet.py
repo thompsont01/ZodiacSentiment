74e39b565b6ca282fcbc3fc74574b7f3924a729a
Trevor Kleinstuber <trevor.kleinstuber@gmail.com>
2020-11-07 11:59:28 -0800

    clean_tweet fianl2

--- clean_tweet.py
+++ clean_tweet.py
@@ -1,4 +1,6 @@
 import re
 def clean_tweet(tweet):
-  s = re.sub(r"(?:\@|https?\://)\S+", "", tweet).replace("#", "")
+  s = re.sub(r".*(?:\@|https?\://)\S+", "", tweet).replace("#", "")
+  #s = re.sub(r"(?:\https?\://)$", "", tweet)
+  s = s.replace('\r',' ').replace('\n',' ')
   return " ".join(s.split())
