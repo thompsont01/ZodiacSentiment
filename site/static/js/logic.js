4cc38a7978ae23945e1e46787646da0e21a9053e
Trevor Kleinstuber <trevor.kleinstuber@gmail.com>
2020-12-07 23:02:54 -0800

    outlined next steps

---
+++ site/static/js/logic.js
@@ -0,0 +1,12 @@
+/*
+data = [ [ sign , yesterday sentiment , today sentiment ]]
+for sign in signs:
+    if today > yesterday:
+        set sign id to visible
+    else:
+        set to invisible
+
+last update = ()
+
+***add server script for twitter listener & sentiment updater
+*/
